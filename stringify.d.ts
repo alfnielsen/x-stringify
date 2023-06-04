91789ded4692e610d9f04bbc8a9113b07f276469
Alf Nielsen <alfnielsen@gmail.com>
2023-06-04 14:34:36 +0200

    Init add test and methods

---
+++ stringify.d.ts
@@ -0,0 +1,7 @@
+export default function stringify(obj: any, options?: {
+    indent?: string;
+    maxDepth?: number;
+    showUndefined?: boolean;
+    showNull?: boolean;
+    leadingComma?: boolean;
+}): string;
